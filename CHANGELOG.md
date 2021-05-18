d82b332b497021a8ab7e736f013b185b411f3c12
luisescobar89 <luis.scobr@gmail.com>
2021-05-18 14:59:27 -0600

    release-0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,26 @@
+# dynatraceoneagent CHANGELOG
+
+All notable changes to this project will be documented in this file.
+
+# 0.1.0
+
+Initial release.
+
+# 0.2.0
+
+Features
+
+- Ability to download specific version
+- Cookbook automatically detects OS and downloads required installer.
+- Cookbook automatically detects OS and will install Dynatrace OneAgent with required Chef resource.
+- Add support for OneAgent Install Params
+- Added parameter to set Dynatrace OneAgent package status
+- Added parameter to set Dynatrace OneAgent service status
+- Introduced OneAgent installer signature verification functionality
+- Add option to delete installer files after successful install.
+- Added idempotency
+
+Bugfixes
+
+- Using cookbook specific attributes
+- Fixed windows installer
--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,19 @@
 name 'dynatraceoneagent'
-maintainer 'Piotr Mejer'
-maintainer_email 'piotr.mejer@dynatrace.com'
+maintainer 'Luis Escobar'
+maintainer_email 'ace@dynatrace.com'
 license 'MIT'
-description 'Installs the Dynatrace One Agent.'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
-source_url 'https://github.com/Dynatrace/Dynatrace-OneAgent-Chef'
+description 'Installs the Dynatrace OneAgent.'
+version '0.2.0'
+chef_version '>= 14.0'
 issues_url 'https://github.com/Dynatrace/Dynatrace-OneAgent-Chef/issues'
-chef_version '>= 12.1'
-%w( debian ubuntu redhat centos fedora amazon windows ).each do |os|
-  supports os
-end
+source_url 'https://github.com/Dynatrace/Dynatrace-OneAgent-Chef'
+
+supports 'amazon'
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'opensuse'
+supports 'oracle'
+supports 'redhat'
+supports 'ubuntu'
+supports 'windows'
